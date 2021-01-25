35843b273fafb132296f5b7a3c1321deff8feee5
hns-dev <hekma.sh@gmial.com>
2021-01-25 21:04:56 +0300

    fixed desktop layout and sliding functionality.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
-const testimonial = document.querySelectorAll('.testimonial');
+const profileImgs = document.querySelectorAll('.profile-pic');
+const quotes = document.querySelectorAll('.quote-block');
 
 prev.addEventListener('click', prevSlide);
 next.addEventListener('click', nextSlide);
 
 function nextSlide(){
-    const active = document.querySelector('.active');
-    active.classList.remove('active');
+    const activeImg = document.querySelector('.active-img');
+    const activeQuote = document.querySelector('.active-quote');
+    activeImg.classList.remove('active-img');
+    activeQuote.classList.remove('active-quote');
 
-    if(active.nextElementSibling){
-        active.nextElementSibling.classList.add('active');
+    if(activeImg.nextElementSibling && activeImg.nextElementSibling.classList.contains('profile-pic')){
+        activeImg.nextElementSibling.classList.add('active-img');
     } else{
-        testimonial[0].classList.add('active');
+        profileImgs[0].classList.add('active-img');
+    }
+
+    if(activeQuote.nextElementSibling){
+        activeQuote.nextElementSibling.classList.add('active-quote');
+    } else{
+        quotes[0].classList.add('active-quote');
     }
 }
 
 function prevSlide(){
-    const active = document.querySelector('.active');
-    active.classList.remove('active');
+    const activeImg = document.querySelector('.active-img');
+    const activeQuote = document.querySelector('.active-quote');
+    activeImg.classList.remove('active-img');
+    activeQuote.classList.remove('active-quote');
 
-    if(active.previousElementSibling){
-        active.previousElementSibling.classList.add('active');
+    if(activeImg.previousElementSibling){
+        activeImg.previousElementSibling.classList.add('active-img');
     } else{
-        testimonial[testimonial.length-1].classList.add('active');
+        profileImgs[profileImgs.length-1].classList.add('active-img');
+    }
+
+    if(activeQuote.previousElementSibling){
+        activeQuote.previousElementSibling.classList.add('active-quote');
+    } else{
+        quotes[profileImgs.length-1].classList.add('active-quote');
     }
 }
